bfcbd983758e6fc42bf3775c615adc06c96fe3d3
KarolRa11 <152220865+KarolRa11@users.noreply.github.com>
2024-02-23 10:16:12 -0600

    Add files via upload

---
+++ expresiones.js
@@ -0,0 +1,8 @@
+function saludar() {
+  const nombre = "UNID";
+  console.log("hola", nombre);
+}
+function saludarDos() {
+  const nombreDos = prompt("Ingres un nombre");
+  console.log("Hola", nombreDos);
+}
