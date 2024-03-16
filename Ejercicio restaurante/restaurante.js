47d65f369b2ad116e0eb9b27acb69d24d95a6fe2
KarolRa11 <152220865+KarolRa11@users.noreply.github.com>
2024-03-15 23:02:09 -0600

    Add files via upload

---
+++ Ejercicio restaurante/restaurante.js
@@ -0,0 +1,9 @@
+var totalcomida=2800 + 1300;
+var totalBebida= 900 +650;
+
+var total=totalcomida + total totalBebida;
+
+console.log("El total de comida es:"+ totalcomida )
+console.log("El total de bebida es:"+ totalBebida )
+console.log("El total de la cuenta es:"+ total )
+
