c931740aaacffc5863c38dd25fdf51d0ceded6d6
KarolRa11 <152220865+KarolRa11@users.noreply.github.com>
2024-06-17 09:35:48 -0600

    Add files via upload

---
+++ Cinepolis/main.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cartelera - Cinépolis</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Cartelera de Películas</h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="main.html">Cartelera</a></li>
+                <li><a href="promociones.html">Promociones</a></li>
+                <li><a href="proximamente.html">Próximamente</a></li>
+                <li><a href="contacto.html">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="movies">
+            <h2>Películas en Cartelera</h2>
+            <ul>
+                <li>
+                    <h3>Mundo encanto</h3>
+                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.disneylatino.com%2Fpeliculas&psig=AOvVaw3fNZphd2IIIR88EKAw7W5B&ust=1718375320372000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJCxp5Ll2IYDFQAAAAAdAAAAABAE" alt="Mundo encanto">
+                    <p>Descripción de Mundo encanto.</p>
+                </li>
+                <li>
+                    <h3>Brave</h3>
+                    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.diezminutos.es%2Fteleprograma%2Fseries-tv%2Fg29017273%2Fpeliculas-para-ver-en-familia%2F&psig=AOvVaw3fNZphd2IIIR88EKAw7W5B&ust=1718375320372000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJCxp5Ll2IYDFQAAAAAdAAAAABAJ" alt="Brave">
+                    <p>Descripción de Brave.</p>
+                </li>
+                <li>
+                    
+                </li>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2024 Cinépolis. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
